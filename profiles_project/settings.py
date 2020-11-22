9504d1dc392eab473166870938c4e4158f95295d
Yasin Amini <Yasin_Amini@outlook.com>
2020-11-22 15:46:20 +0100

    configured for deplyment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*jix60s*3)s=ps&&e%_flhwsorsvb^806v9&@_kqeg*4q8n_tf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL= 'profile_api.UserProfile'
+STATIC_ROOT='static/'
